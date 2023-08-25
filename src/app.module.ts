675f306cea9b1437b5380e7a0ac1f05e516cd548
Natanael Signorini <natanaelsignorini01@gmail.com>
2023-08-25 17:16:28 -0300

    🔧 updating connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        database: 'teste-backend-brainny',
+        username: 'postgres',
+        password: 'postgres',
+        entities: ['**/entities/*.js,modules/**/entities/*.js'],
+        migrations: ['src/database/migrations/*{.ts,.js}'],
+        autoLoadEntities: true,
+        logging: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
