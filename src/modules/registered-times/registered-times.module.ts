5b37d27546f1d4b2818cac05a5de5e7a21621bb4
Natanael Signorini <natanaelsignorini01@gmail.com>
2023-08-26 23:29:36 -0300

    🔨 udpate

--- src/modules/registered-times/registered-times.module.ts
+++ src/modules/registered-times/registered-times.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RegisteredTimesService } from './registered-times.service';
 import { RegisteredTimesResolver } from './registered-times.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RegisteredTimes } from './entities/registered-times.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RegisteredTimes])],
   providers: [RegisteredTimesResolver, RegisteredTimesService],
 })
 export class RegisteredTimesModule {}
